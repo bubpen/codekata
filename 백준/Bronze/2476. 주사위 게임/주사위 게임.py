f0fece8fb7aeb6e8d111478ddcdb3fdfe1b7d14f
bubpen <shghtjd711@naver.com>
2024-11-21 12:01:04 +0900

    [Bronze III] Title: 주사위 게임, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2476. 주사위 게임/주사위 게임.py
+++ 백준/Bronze/2476. 주사위 게임/주사위 게임.py
@@ -2,13 +2,11 @@ N = int(input())
 result = []
 for i in range(N):
     l = list(map(int,input().split()))
-    if l[0]==l[1] and l[0]==l[2]:
+    if l.count(l[0])==3:
         result.append(10000+l[0]*1000)
-    elif l[0] == l[1]:
+    elif l.count(l[0])==2:
         result.append(1000+l[0]*100)
-    elif l[0]==l[2]:
-        result.append(1000+l[0]*100)
-    elif l[1]==l[2]:
+    elif l.count(l[1]) == 2:
         result.append(1000+l[1]*100)
     else:
         result.append(max(l)*100)
