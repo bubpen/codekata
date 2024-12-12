a3245d8f3c1ccf7baf9cc6c6ea9296fdce362a3c
bubpen <shghtjd711@gmail.com>
2024-12-12 10:18:29 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.py
+++ 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.py
@@ -1,7 +1,2 @@
 def solution(my_string, n):
-    i = -1
-    answer = ''
-    while i >= -n:
-        answer = my_string[i] + answer
-        i -= 1
-    return answer
+    return my_string[len(my_string)-n:]
