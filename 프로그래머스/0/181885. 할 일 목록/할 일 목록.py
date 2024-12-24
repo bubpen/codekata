1d527cec95c8445a2b7993ed5ebdbab719c33717
bubpen <shghtjd711@gmail.com>
2024-12-24 15:42:24 +0900

    [level 0] Title: 할 일 목록, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -1,6 +1,2 @@
 def solution(todo_list, finished):
-    answer = []
-    for i in range(len(todo_list)):
-        if not finished[i]:
-            answer.append(todo_list[i])
-    return answer
+    return [t for i,t in enumerate(todo_list) if not finished[i]]
