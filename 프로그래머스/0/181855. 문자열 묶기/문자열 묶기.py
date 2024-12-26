ac94855871bb59a979dd9fe17461512983e38ace
bubpen <shghtjd711@gmail.com>
2024-12-26 10:20:52 +0900

    [level 0] Title: 문자열 묶기, Time: 58.08 ms, Memory: 17.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
@@ -0,0 +1,12 @@
+def solution(strArr):
+    length = []
+    for i in strArr:
+        length.append(len(i))
+    i =1
+    answer = 0
+    while i in length:
+        if answer < length.count(i):
+            answer = length.count(i)
+        i += 1
+    
+    return answer
