f8a98f8796c0e70496ee8338ee3fd1c87ecb6c03
bubpen <shghtjd711@gmail.com>
2024-12-30 09:33:09 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+SELECT concat('/home/grep/src/', F.board_id, '/',file_id, file_name, file_ext) FILE_PATH
+FROM USED_GOODS_BOARD B JOIN USED_GOODS_FILE F ON B.BOARD_ID = F.BOARD_ID
+WHERE VIEWS =
+(
+SELECT MAX(VIEWS)
+FROM USED_GOODS_BOARD
+)
+ORDER BY FILE_ID DESC
