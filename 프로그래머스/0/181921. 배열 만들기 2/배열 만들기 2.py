dbc0b34042145e57afcf53aa3a20394aab824e52
bubpen <shghtjd711@gmail.com>
2025-01-31 09:02:39 +0900

    [level 0] Title: 배열 만들기 2, Time: 464.96 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,13 +1,3 @@
 def solution(l, r):
-    answer = []
-    for i in range(l, r+1):
-        for w in str(i):
-            t = True
-            if w in '12346789':
-                t = False
-                break    
-        if t:
-            answer.append(i)
-    if len(answer) == 0:
-        answer.append(-1)
-    return answer
+    answer = [i for i in range(l, r+1) if all(w in '05' for w in str(i))]
+    return answer if answer else [-1]
