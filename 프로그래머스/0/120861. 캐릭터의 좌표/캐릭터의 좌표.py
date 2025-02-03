aba0957651e6a82ca5946ef6d1810645e8a79270
bubpen <shghtjd711@gmail.com>
2025-02-03 09:07:47 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.02 ms, Memory: 9.92 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -1,17 +1,17 @@
 def solution(keyinput, board):
     position = [0,0]
     a,b =board[0]//2,board[1]//2
-    for i in keyinput:
-        if i == 'left':
+    for key in keyinput:
+        if key == 'left':
             if position[0] != -a:
                 position[0] -= 1
-        if i == 'right':
+        if key == 'right':
             if position[0] != a:
                 position[0] += 1
-        if i == 'up':
+        if key == 'up':
             if position[1] != b:
                 position[1] +=1
-        if i == 'down':
+        if key == 'down':
             if position[1] != -b:
                 position[1] -= 1
     return position
