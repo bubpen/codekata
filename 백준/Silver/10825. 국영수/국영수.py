9cd33fd452230fe228a8c60912bc6576b52ae762
bubpen <shghtjd711@gmail.com>
2025-04-17 11:57:30 +0900

    [Silver IV] Title: 국영수, Time: 392 ms, Memory: 63432 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.py
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -1,8 +1,13 @@
-n = int(input())
-array = []
-for i in range(n):
-    name, korean, english, math = input().split()
-    array.append((name, int(korean), int(english), int(math)))
-array.sort(key = lambda x: (-x[1], x[2], -x[3], x[0]))
-for i in array:
-    print(i[0])
+import sys
+
+n = int(sys.stdin.readline())
+students = []
+
+for _ in range(n):
+    name, kor, eng, math = sys.stdin.readline().split()
+    students.append((name, int(kor), int(eng), int(math)))
+
+students.sort(key=lambda x: (-x[1], x[2], -x[3], x[0]))
+
+for student in students:
+    print(student[0])
