a73eee1e716bc8ad5937f45e448b904743ce0d05
bubpen <shghtjd711@gmail.com>
2025-12-14 14:35:41 +0900

    [level 0] Title: 코드 처리하기, Time: 368.44 ms, Memory: 380 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        int mode = 0;
+        for (int idx = 0;idx < code.length(); idx++) {
+            if (mode == 0) {
+                if (code.charAt(idx) == '1') {
+                    mode = 1;
+                } else {
+                    if (idx % 2 == 0) {answer += code.charAt(idx);}
+                }
+            } else {
+                if (code.charAt(idx) == '1') {
+                    mode = 0;
+                } else {
+                    if (idx % 2 == 1) {answer += code.charAt(idx);}
+                    
+                }
+            }
+        }
+        if (answer.length() == 0) {answer = "EMPTY";}
+        return answer;
+    }
+}
