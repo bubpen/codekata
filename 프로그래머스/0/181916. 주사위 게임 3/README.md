05074d3a2991a58ea28abd300d761a4e8a7819dd
bubpen <shghtjd711@gmail.com>
2026-01-19 17:26:50 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.92 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/README.md
+++ 프로그래머스/0/181916. 주사위 게임 3/README.md
@@ -1,10 +1,10 @@
 # [level 0] 주사위 게임 3 - 181916 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181916) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181916?language=java) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.01 ms
+메모리: 75.7 MB, 시간: 0.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 19일 12:06:47
+2026년 01월 19일 17:26:46
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int a, int b, int c, int d) {
+        int answer = 0;
+        int[] dice = {a, b, c, d};
+        Arrays.sort(dice);
+        if (dice[0] == dice[3]) {
+            return 1111* dice[0];
+        }
+        else if (dice[0] == dice[2] || dice[1] == dice[3]) {
+            int p = dice[1];
+            int q = (dice[0] == dice[2]) ? dice[3] : dice[0];
+            return (int) Math.pow(10 * p + q, 2);
+        }
+        else if (dice[0] == dice[1] && dice[2] == dice[3]) {
+            return (dice[0] + dice[2]) * (Math.abs(dice[0] - dice[2]));
+        }
+        else if (dice[0] == dice[1] || dice[1] == dice[2] || dice[2] == dice[3]) {
+            if (dice[0] == dice[1]) return dice[2] * dice[3];
+            if (dice[1] == dice[2]) return dice[0] * dice[3];
+            return dice[0] * dice[1];
+        }
+        else {
+            return dice[0];
+        }
+    }
+}
